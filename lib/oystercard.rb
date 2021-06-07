398ff43381d7b67d71cb2649dd5d6e2a67af72fc
Juel07 <juel.braganza97@gmail.com>
2021-06-08 00:21:50 +0100

    Closing #7: Deduct money from balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,25 @@
-MAX_LIMIT = 90
-
 class Oystercard
   attr_reader :balance
+
+  MAX_LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(money)
-    raise "Pound 90 limit reached!" if @balance >= MAX_LIMIT
+    max_topup_limit
     @balance += money
+  end
+
+  def deduct(money)
+    @balance -= money
+  end
+
+  private
+
+  def max_topup_limit
+    raise "Pound #{MAX_LIMIT} limit reached!" if @balance >= MAX_LIMIT
   end
 end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,14 +5,23 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
-  it "can top up the balance" do
-    # expect(subject.top_up(10)).to eq(10)
-    expect { subject.top_up 10 }.to change { subject.balance }.by 10
+  describe "#top_up" do
+    it "can top up the balance" do
+      # expect(subject.top_up(10)).to eq(10)
+      expect { subject.top_up 10 }.to change { subject.balance }.by 10
+    end
+    it "raise error if max limit reached" do
+      max_limit = Oystercard::MAX_LIMIT
+      error_msg = "Pound #{max_limit} limit reached!"
+      subject.top_up(max_limit)
+      expect { subject.top_up(10) }.to raise_error(error_msg)
+    end
   end
 
-  it "raise error if max limit reached" do
-    error_msg = "Pound 90 limit reached!"
-    subject.top_up(90)
-    expect { subject.top_up(10) }.to raise_error(error_msg)
+  describe "#deduct" do
+    it "can deduct from balance" do
+      subject.top_up(20)
+      expect { subject.deduct 10 }.to change { subject.balance }.by -10
+    end
   end
 end
